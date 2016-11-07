bb0a215c2ad828d40c2b539cdea275cfa54a4495
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-11-07 21:27:26 +0300

    test commit

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,6 +3,7 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
+    test_commit = 10
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant < 0:
