d821b32012695c06771f26a50926624e7af259e4
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-11-07 21:37:46 +0300

    test hook

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    test_commit = 5
+    test_commit = 'test'
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant < 0:
